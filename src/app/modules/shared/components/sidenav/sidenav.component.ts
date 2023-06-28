2153b515ed9d65c31392fd371117fa0baef6cdcc
btista-b013m <btista-b013m@kinal.edu.gt>
2023-06-28 00:51:25 -0600

    Finalizando el dashboard del aplicativo

---
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -0,0 +1,25 @@
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.css']
+})
+export class SidenavComponent implements OnInit {
+
+  mobileQuery: MediaQueryList
+
+  menuNav =[
+    {name:'Home', router:'home', icon:'home'},
+    {name:'Carreras', router:'home', icon:'category'},
+    {name:'Examenes', router:'home', icon:'calendar_today'},
+  ];
+
+  ngOnInit(): void { 
+  }
+
+  constructor(media: MediaMatcher){
+    this.mobileQuery= media.matchMedia('(max-witdth: 600px)');
+  }
+}
